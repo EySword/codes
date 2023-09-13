673e8e0b0321182bdae02a25a497ece783a2d401
EySword <yche123@qq.com>
2023-09-13 16:44:02 +0800

    update

--- prolog/learn/three.pl
+++ prolog/learn/three.pl
@@ -12,3 +12,27 @@ times(s(X),Y,Z) :- plus(W,Y,Z), times(X,Y,W).
 
 mod(X,Y,X) :- X<Y.
 mod(X,Y,Z) :- plus(X1,Y,X), mod(X1,Y,Z).
+
+delete([],X,[]).
+delete([X|Xs],X,Y) :- delete(Xs,X,Y).
+delete([X|Xs],Z,[X|Y]) :- delete(Xs,Z,Y).
+
+select(X,[X|Xs],Xs).
+select(X,[Z|Zs],[Z|Y]) :- select(X,Zs,Y).
+
+insert(X,Ys,Zs) :- select(X,Zs,Ys).
+
+sort(Xs,Ys) :- permutation(Xs,Ys), write(Ys), ordered(Ys).
+
+ordered([X]).
+ordered([X,Y|Z]) :- X=<Y, ordered([Y|Z]).
+
+permutation(Xs,[Z|Zs]) :- select(Z,Xs,Ys), permutation(Ys,Zs).
+permutation([],[]).
+
+binary_tree(void).
+binary_tree(tree(Element,Left,Right)) :- binary_tree(Left), binary_tree(Right).
+
+tree_member(X,tree(X,Left,Right)).
+tree_member(X,tree(Element,Left,Right)) :- tree_member(X,Left).
+tree_member(X,tree(Element,Left,Right)) :- tree_member(X,Right).
