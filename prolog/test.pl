d75c3451f6c02a9e3e9fe7150fa3e3a1bbe337e5
EySword <yche123@qq.com>
2023-09-17 23:48:32 +0800

    1

--- prolog/test.pl
+++ prolog/test.pl
@@ -1,27 +1,5 @@
-plus(X,Y,Z):-Z is X+Y.
-plus2(X,Y):- 3 is X+Y.
+select(X,[X|Xs],Xs) :- write('1').
+select(X,[Y|Ys],[Y|Zs]) :- write('2'), select(X,Ys,Zs).
 
-recrusion(N,F):-recrusion(N,1,F).
-recrusion(N,T,F):- N>0, T1 is T*N, N1 is N-1, recrusion(N1,T1,F).
-recrusion(0,F,F).
-
-data(one).
-data(two).
-data(three).
-data(four).
-
-no_cut(X) :- 
-    data(X), write(X), tab(3), write('here'),nl, fail.
-no_cut(X) :- write('End').
-
-do_cut(X) :- data(X), write(X), tab(3), !, write('here'), nl, fail.
-do_cut('End').
-
-no_cut_2(X,Y) :- 
-    data(X), data(Y).
-no_cut_2(_X,_Y) :- write('End').
-
-do_cut_2(X,Y) :- 
-    data(X), !, data(Y).
-do_cut_2(_X,_Y) :- write('End').
-
+maximum(X,Y,X) :- X >= Y.
+maximum(X,Y,Y) :- Y >= X.
