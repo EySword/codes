3b92f37ee615462f24d72c540d094ecd709a93ef
EySword <yche123@qq.com>
2023-12-07 11:42:19 +0800

    0

--- prolog/test.pl
+++ prolog/test.pl
@@ -1,4 +1,7 @@
+:- use_module('test2').
+
 metarule([P,Q], [P,A,B], [[Q,A,B]]).
 metarule([P,Q,R], [P,A,B], [[Q,A,C],[R,C,B]]).
 
-user:term_expansion(metarule(Subs,Head,Body),metarule('name',Subs,Head,Body,'rec','out')).
+% term_expansion(metarule(Subs,Head,Body),metarule(Subs,Head,Body,'rec')) :-
+%     write('=====\n').
---
+++ prolog/test2.pl
@@ -0,0 +1,2 @@
+user:term_expansion(metarule(Subs,Head,Body),test2:metarule(Subs,Head,Body,'rec')) :-
+    write('=====\n').
